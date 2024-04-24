438017ad8a827d0fd39ef749bdf6032f991bcfc3
matvey <mogilyoy@ya.ru>
2024-04-24 23:43:28 +0500

    admin first try

--- public/admin.php
+++ public/admin.php
@@ -24,5 +24,19 @@
 
     <!-- AdminLTE JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/js/adminlte.min.js"></script>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  File name:
+  <input type="text" name="fileName" id="fileName">
+  Description:
+  <input type="text" name="description" id="description">
+  User ID:
+  <input type="number" name="user_id" id="user_id" value="0">
+
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
   </body>
 </html>
---
+++ public/upload.php
@@ -0,0 +1,74 @@
+<?php
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+
+// Get post data
+$fileName = $_POST['fileName'];
+$description = $_POST['description'];
+$author = $_POST['user_id'];;
+
+// Get extension of the file
+$imageFileType = strtolower(pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
+
+// Hash the file name and append the extension
+$hashedFileName = hash('sha256', $fileName . time()) . '.' . $imageFileType;
+
+$target_dir = "uploads/";
+$target_file = $target_dir . $hashedFileName;
+
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+
+  $stmt = $dbh->prepare("INSERT INTO plants (user_id, title, description, filename)
+                       VALUES (:user_id, :title, :description, :filename");
+  $stmt->bindParam(':user_id', $fileName);
+  $stmt->bindParam(':title', $description);
+  $stmt->bindParam(':description', $description);
+  $stmt->bindParam(':filename', $hashedFileName);
+
+  $stmt->execute();
+
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". basename($hashedFileName). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+?>
