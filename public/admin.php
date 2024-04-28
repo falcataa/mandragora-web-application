7905fed5841c1f94a04f1b109b20b2d81cc24bd2
matvey <mogilyoy@ya.ru>
2024-04-28 23:49:53 +0500

    css changed

--- public/admin.php
+++ public/admin.php
@@ -12,7 +12,9 @@ if (!isset($_SESSION['admin'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Admin Panel</title>
+
   <link rel="stylesheet" type="text/css" href="./css/admin-style.css?<?php echo uniqid(); ?>">
+  <link rel="stylesheet" type="text/css" href="./css/normalize.css">
 
 <script>
   const urlParams = new URLSearchParams(window.location.search);
