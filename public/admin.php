29483bf32606a377165a8b518b4ad02e8b6556c8
matvey <mogilyoy@ya.ru>
2024-04-28 21:28:45 +0500

    admin.php updated

--- public/admin.php
+++ public/admin.php
@@ -8,8 +8,11 @@ if (!isset($_SESSION['admin'])) {
 <!DOCTYPE html>
 <html>
 <head>
-        <title>Admin Panel</title>
-        <link rel="stylesheet" type="text/css" href="./css/admin-style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Admin Panel</title>
+  <link rel="stylesheet" type="text/css" href="./css/admin-style.css">
 
 <script>
 let stepCount = 2;
@@ -23,7 +26,7 @@ function addTransplantationStep() {
     <input type="file" name="TransplantationImage${stepCount}" id="TransplantationImage${stepCount}">
 
     Описание ${stepCount} пересадки:
-    <input type="text" name="TransplantationDescription${stepCount}" id="TransplantationDescription${stepCount}" style="width:200px; height:50px;">
+    <input type="textarea" name="TransplantationDescription${stepCount}" id="TransplantationDescription${stepCount}" style="width:200px; height:50px;">
     `;
 
     transplantationStepsDiv.appendChild(newStepDiv);
@@ -31,10 +34,35 @@ function addTransplantationStep() {
     stepCount++;
 }
 </script>
+
+<script>
+$(document).ready(function() {
+    $('#uploadPlantFrom').submit(function(e) {
+        e.preventDefault(); // Предотвращаем стандартное поведение формы
+
+        $.ajax({
+            type: 'POST',
+            url: 'upload.php', // Укажите путь к вашему обработчику формы
+            data: $(this).serialize(),
+            success: function(response) {
+                // Обработка успешного ответа от сервера
+                console.log(response);
+                // Можно добавить здесь логику для обновления части страницы или вывода сообщения об успешной отправке
+            },
+            error: function(xhr, status, error) {
+                // Обработка ошибок
+                console.error(error);
+            }
+        });
+    });
+});
+</script>
+
+
 </head>
 
   <body>
-  <form action="upload.php" method="post" enctype="multipart/form-data" class="center-form">
+  <form id="uploadPlantFrom" enctype="multipart/form-data" class="center-form">
     Добавьте фото растения (выберите сразу все фотографии):
     <input type="file" name="fileToUpload[]" id="fileToUpload" multiple>
 
@@ -42,22 +70,22 @@ function addTransplantationStep() {
     <input type="text" name="flowerName" id="flowerName">
 
     Описание:
-    <input type="text" name="flowerDescription" id="flowerDescription" style="width:200px; height:50px;">
+    <input type="textarea" name="flowerDescription" id="flowerDescription" style="width:200px; height:50px;">
 
     Происхождение:
-    <input type="text" name="plantOrigin" id="plantOrigin" style="width:200px; height:50px;">
+    <input type="textarea" name="plantOrigin" id="plantOrigin" style="width:200px; height:50px;">
 
     Уход за растением:
-    <input type="text" name="plantCare" id="plantCare" style="width:200px; height:50px;">
+    <input type="textarea" name="plantCare" id="plantCare" style="width:200px; height:50px;">
 
     Научные данные:
-    <input type="text" name="scientificData" id="scientificData" style="width:200px; height:50px;">
+    <input type="textarea" name="scientificData" id="scientificData" style="width:200px; height:50px;">
 
     Пересадка шаг 1:
     <input type="file" name="TransplantationImage1" id="TransplantationImage1">
 
     Описание 1 пересадки:
-    <input type="text" name="TransplantationDescription1" id="TransplantationDescription1" style="width:200px; height:50px;">
+    <input type="textarea" name="TransplantationDescription1" id="TransplantationDescription1" style="width:200px; height:50px;">
 
     <div id="transplantationSteps"></div>
 
