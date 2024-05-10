dd2b43682356f57f4a1c798c57bd6afd4ef14c10
siyazoff <83133133+siyazoff@users.noreply.github.com>
2024-05-10 21:53:45 +0500

    edit plug to db

--- public/includes/config.php
+++ public/includes/config.php
@@ -1,14 +1,14 @@
 <?php
 // Параметры подключения к базе данных
-// define('DB_SERVER', 'localhost'); // Адрес сервера базы данных, обычно localhost
-// define('DB_USERNAME', 'dev'); // Имя пользователя базы данных
-// define('DB_PASSWORD', 'DarkhanBestProgrammerSuckHisDIck!!_27'); // Пароль пользователя базы данных
-// define('DB_DATABASE', 'mandragora'); // Имя базы данных
-
 define('DB_SERVER', 'localhost'); // Адрес сервера базы данных, обычно localhost
-define('DB_USERNAME', 'root'); // Имя пользователя базы данных
-define('DB_PASSWORD', 'root'); // Пароль пользователя базы данных
-define('DB_DATABASE', 'user_registration'); // Имя базы данных
+define('DB_USERNAME', 'dev'); // Имя пользователя базы данных
+define('DB_PASSWORD', 'DarkhanBestProgrammerSuckHisDIck!!_27'); // Пароль пользователя базы данных
+define('DB_DATABASE', 'mandragora'); // Имя базы данных
+
+// define('DB_SERVER', 'localhost'); // Адрес сервера базы данных, обычно localhost
+// define('DB_USERNAME', 'root'); // Имя пользователя базы данных
+// define('DB_PASSWORD', 'root'); // Пароль пользователя базы данных
+// define('DB_DATABASE', 'user_registration'); // Имя базы данных
 
 // Подключение к базе данных
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
